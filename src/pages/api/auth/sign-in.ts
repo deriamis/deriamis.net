789b146d8a333f9d31b794e695a3976473927d1e
Ryan Egesdahl <deriamis@houdini.local>
2025-11-07 11:01:03 -0800

    Remove remaining auth page missed in last commit

--- src/pages/api/auth/sign-in.ts
+++
@@ -1,6 +0,0 @@
-import { authClient } from "@lib/auth-client";
-
-const response = await authClient.signIn.oauth2({
-  providerId: "instagram",
-  callbackURL: "/dashboard", // the path to redirect to after the user is authenticated
-});
